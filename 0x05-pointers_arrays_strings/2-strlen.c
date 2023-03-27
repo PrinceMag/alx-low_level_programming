3d6eab7758e7c0af1246cb5bdc8f0c7fc0284864
princemag <princeth.magutshwa@gmail.com>
2023-03-27 07:41:27 -0700

    function that returns the length of a string.

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -12,8 +12,8 @@ int _strlen(char *s)
         
     while (*s != '\0')
     {
-    	n = n + 1;
-    	s = s + 1; /* La direccion de s aumenta en 1 */
+    n = n + 1;
+    s = s + 1; /* La direccion de s aumenta en 1 */
     }
         
     return(n); /* Retorna el valor final de n */
